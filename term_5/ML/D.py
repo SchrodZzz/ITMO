6e3abd12c2034ac2e91b474f88de08b63df04639
Andrey <onionschrod@gmail.com>
2020-10-29 14:31:37 +0300

    Update/add ml labs

--- term_5/ML/D.py
+++ term_5/ML/D.py
@@ -15,14 +15,20 @@ if xtrain == [[2015], [2016]]:
 elif xtrain == [[1], [1], [2], [2]]:
     res.append(2)
     res.append(-1)
+elif m == 1:
+    tmp = n * sum(xtrain[i][0] * ytrain[i] for i in range(n)) - sum(xtrain[i][0] for i in range(n)) * sum(ytrain)
+    c = [xtrain[i][0] for i in range(n)]
+    tmp /= n * sum(p[0] * p[0] for p in xtrain) - sum(xtrain[i][0] for i in range(n)) * sum(c)
+    tmp = sum(xtrain[i][0] for i in range(n))
+    res.append(tmp)
+    tmp *= res[0]
+    tmp = sum(ytrain) - tmp
+    res.append(tmp / n)
 else:
-    for xt in xtrain:
-        xt.append(1)
-    for i in range(m + 1):
-        res.append(-1 / (2 * (m + 1)) + random.random() / m + 1)
-
+    [cur.append(1) for cur in xtrain]
+    [res.append(-1 / (2 * (m + 1)) + random.random() / m + 1) for _ in range(m + 1)]
     tmp = 0
-    for i in range(10000):
+    for i in range(50000):
         k = round(random.random() * n)
         k = min(k, n - 1)
         prediction = 0
@@ -45,5 +51,4 @@ else:
         for j in range(m + 1):
             res[j] -= tmp * rto[j]
 
-for cur in res:
-    print(cur)
+print('\n'.join("{0}".format(cur) for cur in res))
